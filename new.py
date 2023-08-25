d7d471bd77f566b672899fabe4fd9d4ac5845724
sowjanya <sowjanyapamulapati55@gmail.com>
2023-08-24 17:06:53 -0700

    Initial commit - Object orientated programming

---
+++ new.py
@@ -0,0 +1,17 @@
+class User:
+    def __init__(self, name, birthyear):
+        self.name = name
+        self.birthyear = birthyear
+    def get_name(self):
+        return self.name.upper()
+    def age(self, current_year):
+        age = current_year - self.birthyear
+        return age
+
+
+user = User("John", 1999)
+
+print(user.age(2023))
+print(user.get_name())
+
+
